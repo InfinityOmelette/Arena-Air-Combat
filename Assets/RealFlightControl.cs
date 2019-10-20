e5bb8bb90d8e229c8ec6e7d148fc81bf3d3c65f7
Paul J <paul.m.jacobson13@gmail.com>
2019-10-19 21:44:19 -0700

    Made more notes...about to step into the breach..

--- Assets/RealFlightControl.cs
+++ Assets/RealFlightControl.cs
@@ -4,31 +4,32 @@ using UnityEngine;
 
 
 /*  
- *  calculateLiftAndInducedDrag()
+ *  create general lift function:
+ *  calculateResultLiftVector(float AoA, float dragCoeff, float liftCoeff, float alphaToLiftOffset, float alphaToDragOffset, Vector3 velocity, Transform referenceTransform)
  *      - LIFT IS PERPENDICULAR TO VELOCITY, NOT FORWARD
  *      - Allows for more consistent thrust/drag ratio
  *      - Magnitude scaled up by alpha (angle of attack), from 0 to 90 (positive or negative)
  *          -   LIFT SCALED LINEARLY WITH ALPHA
  *          -   DRAG SCALED EXPONENTIALLY WITH ALPHA
+ *          -   0 ALPHA WILL CREATE SOME LIFT/DRAG, DRAG WILL NEVER BE ZERO --> OFFSET ALPHA 
  *      - To get Alpha: 
  *          // *flip sign(s) if necessary*
  *          var localVelocity = transform.InverseTransformDirection(rb.velocity);
  *          var angleOfAttack = Mathf.Atan2(-localVelocity.y, localVelocity.z);
  *      - Two force vectors
- *          - Lift = liftCoeff * (velocity^2) * alpha * Cross(transform.right, velocity).normalized
- *          - Drag = dragCoeff * (velocity^2) * (alpha^2) * (-velocity.normalized)
- *          
+ *          - Lift = ((liftCoeff * alpha) + alphaToLiftOffset) * (velocity^2) * Cross(transform.right, velocity).normalized
+ *          - Drag = ((dragCoeff * alpha)^2 + alphaToDragOffset) * (velocity^2) * (-velocity.normalized)
+ *  
+ *  
+ *  calculateLiftsAndDrags() -- compensates for both induced and parasitic drag
  *          WEIRD FUCKIN IDEA -- TWO "LIFT" VECTORS CALCULATED THE SAME WAY? (Up lift and side lift)
- *              - AXIS RELATIVE TO VELOCITY
+ *              AXES RELATIVE TO VELOCITY
+ *              - WING LIFT
  *              - "SIDE" LIFT
- *              - Plus standard 
  *  
  *  
- *  calculateAxisParasiticDrag()
- *      - Three drag axes - axis drag = (axisVel)^2 * (axis drag coeff)
- *          1. Longitudinal (fwd/back)
- *          2. Lateral (left/right)
- *          3. Vertical (up/down) -- DOES NOT INCLUDE WINGS. ONLY FUSELAGE DRAG
+ *  calculateAxisParasiticDrag() -- use only for drag items, not lifting bodies
+ *      - 
  *          
  *          
  *  aceCombatThrottleProcess()
@@ -36,9 +37,11 @@ using UnityEngine;
  *      - Thrust can change rapidly -- constant thrustDelta steps
  *      - MAYBE include afterburner stage? multiplies thrustDelta by x above thrust y
  *  
+ *  processBaseControlTorque()
+ *      - 
  *  
  *  calculateControlAuthorityByThrust() --> change to by FORWARD velocity
- *      - Set optimal turn rate to be at level speed with cruise thrust
+ *      - Set cruise thrust to be at optimal turn speed in level flight (mess with flight variables until it's at 30-50% thrust
  *      - Decreases linearly specified rate above corner velocity
  *      
  *      
@@ -48,16 +51,11 @@ using UnityEngine;
  *          - vertical slip pitch
  *          - lateral slip yaw
  *          - lateral slip roll
- *          --> axis velocity * axis stability ratio * normalized cross vector
+ *          --> (axis velocity)^2 * axis stability ratio * normalized cross vector
  *      - Increased angle of attack will increase stability torque
  *  
  * 
  *  processSlipTorques() -- made obselete by stability torque
- *      - Remains mostly the same
- *      - Scale down as speed increases 
- *          (slow speed = high slip, high speed = less slip)
- *      >>> "STALLING" WILL OCCUR WHEN SPEED IS SO SLOW THAT SLIP TORQUE
- *          IS STRONGER THAN CONTROL AUTHORITY
  * 
  */
 
@@ -74,4 +72,6 @@ public class RealFlightControl : MonoBehaviour
     {
         
     }
+
+
 }

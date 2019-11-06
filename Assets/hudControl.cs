9f4928dd7ea9c00646dd57c97498cf0fa34c0a2c
Paul J <paul.m.jacobson13@gmail.com>
2019-11-06 01:53:25 -0800

    Created simple HUD
    Text for speed, thrust, altitude, and vert speed

---
+++ Assets/hudControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hudControl : MonoBehaviour
+{
+
+
+    public Text speedText;
+    public Text altitudeText;
+    public Text throttletext;
+    public Text climbText;
+
+    private Rigidbody rbRef;
+    private RealFlightControl flightInfoObjRef;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbRef = GetComponent<Rigidbody>();
+        flightInfoObjRef = GetComponent<RealFlightControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speedText.text = "Speed: " + Mathf.RoundToInt(rbRef.velocity.magnitude).ToString() + "m/s";
+        throttletext.text = "Thrust: " + Mathf.RoundToInt(flightInfoObjRef.currentThrustPercent).ToString() + "%";
+
+        altitudeText.text = "Altitude: " + Mathf.RoundToInt(transform.position.y).ToString() + "m";
+        climbText.text = "Climb: " + Mathf.RoundToInt(flightInfoObjRef.readVertVelocity).ToString() + "m/s";
+        
+    }
+}

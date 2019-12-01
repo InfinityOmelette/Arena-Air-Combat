d1090d79a4d43931cf89d69e6abecc048409a654
InfinityOmelette <paul.m.jacobson13@gmail.com>
2019-11-30 21:55:09 -0800

    Spectate Cam and CamManager

--- Assets/CamerasManager.cs
+++ Assets/CamerasManager.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class CamerasManager : MonoBehaviour
 {
+
+    public GameObject[] cameras;
+    public GameObject uiRef;
+    public short activeCamIndex;
+
+    public PlayerInput_Aircraft aircraftInputRef;
+
+    public bool showUI;
+    public bool aircraftInputActive;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        enableCamIndex(activeCamIndex, showUI, aircraftInputActive); // enable first camera in list
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            if(activeCamIndex == 0)
+            {
+                activeCamIndex = 1; // show airplane perspective
+                showUI = true;
+                aircraftInputActive = true;
+            }
+            else
+            {
+                activeCamIndex = 0; // show spectator cam
+                showUI = false;
+                aircraftInputActive = false;
+            }
+
+            enableCamIndex(activeCamIndex, showUI, aircraftInputActive);
+        }
     }
+
+    void enableCamIndex(short index, bool showUI, bool enableAircraftControl)
+    {
+
+        cameras[index].SetActive(true); // activate proper camera
+
+        for (short i = 0; i < cameras.Length; i++)
+        {
+            if(i != index)
+                cameras[i].SetActive(false); // deactivate all cameras that aren't specified cam
+        }
+
+        uiRef.SetActive(showUI);
+        aircraftInputRef.enabled = enableAircraftControl;
+    }
+
+    
 }
---
+++ Assets/SpectateCam.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpectateCam : MonoBehaviour
+{
+
+    public float yawRateMod;
+    public float pitchRateMod;
+
+    public float fwdSpeed;
+    public float strafeSpd;
+    public float vertSpd;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currentYawRate = yawRateMod * Input.GetAxis("Mouse X");
+        float currentPitchRate = -pitchRateMod * Input.GetAxis("Mouse Y");
+
+        transform.localEulerAngles += new Vector3(currentPitchRate, currentYawRate, 0.0f);
+
+
+
+        transform.position += (transform.forward * fwdSpeed * Input.GetAxis("Pitch") + 
+            transform.right * strafeSpd * Input.GetAxis("Roll") + 
+            transform.up * vertSpd * Input.GetAxis("Throttle")) * Time.deltaTime;
+
+    }
+}

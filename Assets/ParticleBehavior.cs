5d01af4d968e33a73148f8edf667e15388a7dcea
InfinityOmelette <paul.m.jacobson13@gmail.com>
2019-12-03 15:55:36 -0800

    Explosions at bullet impact

---
+++ Assets/ParticleBehavior.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleBehavior : MonoBehaviour
+{
+
+
+    private ParticleSystem pSystem;
+    private ParticleCollisionEvent[] collisionEvents;
+
+    public long collisionCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pSystem = GetComponent<ParticleSystem>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnParticleCollision(GameObject other)
+    {
+        int collCount = pSystem.GetSafeCollisionEventSize();
+
+        if(collisionEvents == null)
+            collisionEvents = new ParticleCollisionEvent[collCount];
+
+        if (collCount > collisionEvents.Length)
+            collisionEvents = new ParticleCollisionEvent[collCount];
+
+
+        int eventCount = pSystem.GetCollisionEvents(other, collisionEvents);
+
+        for(int i = 0; i < eventCount; i++)
+        {
+            Explosion.createExplosionAt(collisionEvents[i].intersection, 3, 0);
+        }
+        
+
+       // ParticlePhysicsExtensions.
+        collisionCount++;
+        
+    }
+
+
+}

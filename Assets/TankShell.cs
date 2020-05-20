46560752ba221926d27ecd9fe09c2a5a7e646db8
InfinityOmelette <paul.m.jacobson13@gmail.com>
2020-05-19 21:42:51 -0700

    Tank Cannon system

---
+++ Assets/TankShell.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankShell : MonoBehaviour
+{
+
+    public bool localOwned;
+
+    public float initSpeed;
+
+    private Rigidbody rb;
+    public GameObject effectsCenter;
+    private ExplodeStats explodeStats;
+
+    public GameObject effectsInit;
+
+    public GameObject effectsObj;
+    public Light effectsLight;
+    public TrailRenderer trail;
+
+    //private bool readyToEmit;
+
+    private float emitDelay;
+
+    //private bool startTrailOn;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        explodeStats = GetComponent<ExplodeStats>();
+    }
+
+    void Start()
+    {
+        effectsObj = GameObject.Instantiate(effectsInit);
+        trail = effectsObj.GetComponent<TrailRenderer>();
+        effectsLight = effectsObj.GetComponent<Light>();
+        effectsObj.transform.position = effectsCenter.transform.position;
+        GameObject.Destroy(effectsInit);
+
+        //trail.emitting = false;
+        rb.velocity = transform.forward * initSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(effectsObj != null)
+        {
+            effectsObj.transform.position = effectsCenter.transform.position;
+
+            //trail.emitting = readyToEmit;
+
+        }
+
+        transform.rotation = Quaternion.LookRotation(rb.velocity, transform.up);
+    }
+
+    public void readyEmit()
+    {
+        //readyToEmit = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(effectsLight != null)
+        {
+            effectsLight.enabled = false;
+        }
+
+        explodeStats.explode(transform.position);
+        GameObject.Destroy(gameObject);
+    }
+
+}
---
+++ Assets/TankTurret.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankTurret : MonoBehaviour
+{
+    public GameObject projectileSpawn;
+
+    // copy this for every shot
+    public GameObject shellSettings;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            fire();
+        }
+    }
+
+    private void setAim()
+    {
+
+    }
+
+
+    private void fire()
+    {
+        // copy variable data over to determine what kind of shell
+        GameObject shell = GameObject.Instantiate(shellSettings);
+        shell.transform.position = projectileSpawn.transform.position;
+        shell.transform.rotation = projectileSpawn.transform.rotation;
+        shell.SetActive(true);
+
+        //shell.GetComponent<TankShell>().readyEmit();
+    }
+}
